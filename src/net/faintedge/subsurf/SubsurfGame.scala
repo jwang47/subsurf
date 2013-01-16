ff80e01a9d9ae460fdf99dd0a5a94acc8cf1e9c0
Jiahua Wang <jiahuawang7@gmail.com>
2013-01-15 21:22:05 -0800

    Minor modifications

--- src/net/faintedge/subsurf/SubsurfGame.scala
+++ src/net/faintedge/subsurf/SubsurfGame.scala
@@ -16,41 +16,41 @@ import net.faintedge.fiber.controls.CircleRender
 
 object SubsurfGame extends Application("subsurf") {
   def main(args: Array[String]) = SubsurfGame.start()
-  
+
+  val backgroundCategory = 1
+  val defaultCategory = 2
+
+  val defaultFilter = new Filter()
+  defaultFilter.categoryBits = defaultCategory
+  defaultFilter.maskBits = backgroundCategory | defaultCategory
+
+  val bgFilter = new Filter()
+  bgFilter.categoryBits = backgroundCategory
+  bgFilter.maskBits = backgroundCategory | defaultCategory
+
   var player: Entity = null
   var tilePage: TilePage = null
 
   override def init(gc: GameContainer) {
     initWorld
     initUi
-    
+
     cam.follow(player)
   }
-  
+
   private def initUi {
     root += new Entity("selector", Array(new TileSelector(tilePage)))
   }
-  
+
   private def initWorld {
-    val backgroundCategory = 1
-    val defaultCategory = 2
-    
-    val defaultFilter = new Filter()
-    defaultFilter.categoryBits = defaultCategory
-    defaultFilter.maskBits = backgroundCategory | defaultCategory
-    
-    val bgFilter = new Filter()
-    bgFilter.categoryBits = backgroundCategory
-    bgFilter.maskBits = backgroundCategory | defaultCategory
-    
     val coinImage = new Image("data/coin.png")
     player = new Entity("player", Array(new Transform(), new Physics(filter = defaultFilter), new ForceControl(), new RectangleForm(10, 10), new RectangleRender()))
-    
+
     val tileData = Array(
       TileInfo(0, "empty", new NullTileRenderer()),
       TileInfo(1, "mud", new ImageTileRenderer(new Image("data/30.png"))),
       TileInfo(2, "stone", new ImageTileRenderer(new Image("data/21.png"))))
-      
+
     val tilePageEntity = new Entity("tile page", Array(new Transform(), new PhysicsTilePage(bgFilter, tileData, 40, 30)))
     val tilePageOption = tilePageEntity.getControl(classOf[TilePage])
     tilePage = tilePageOption.get
@@ -59,8 +59,8 @@ object SubsurfGame extends Application("subsurf") {
         tilePage.setTile(row, col, 1)
       }
     }
-    
-    root += new Entity("coin", Array(new Transform(), new Physics(fixedRotation = true, filter = defaultFilter), new CircleForm(coinImage.getHeight/2), new ImageRender(coinImage)))
+
+    root += new Entity("coin", Array(new Transform(), new Physics(fixedRotation = true, filter = defaultFilter), new CircleForm(coinImage.getHeight / 2), new ImageRender(coinImage)))
     root += new Entity("test box", Array(new Transform(0, 0, (Math.Pi / 4).toFloat), new Physics(filter = defaultFilter), new RectangleForm(10, 10), new RectangleRender(Color.white)))
     root += new Entity("test circle", Array(new Transform(), new Physics(filter = defaultFilter), new CircleForm(10), new CircleRender(Color.white)))
     root += player
